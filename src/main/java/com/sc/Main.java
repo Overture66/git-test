26b95dd3c03894ca51178250524844a1e0fbab36
Overture <chaoccc2022@163.com>
2024-01-20 21:04:44 +0800

    second-commit

--- src/main/java/com/sc/Main.java
+++ src/main/java/com/sc/Main.java
@@ -4,7 +4,9 @@ package com.sc;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        System.out.println("hello-git");
 
     }
 }
