55b19db22f435b628442e28a5caddde05c0cc279
Overture66 <157210950+Overture66@users.noreply.github.com>
2024-01-21 09:51:45 +0800

    Update Main.java

--- src/main/java/com/sc/Main.java
+++ src/main/java/com/sc/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("hello-git hello");
         System.out.println("hello dev4");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
