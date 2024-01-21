16bdda75088ff3dd23062e6ecfb3d9b4e061f6cf
Overture <chaoccc2022@163.com>
2024-01-21 09:47:36 +0800

    push commit

--- src/main/java/com/sc/Main.java
+++ src/main/java/com/sc/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("hello-git");
         System.out.println("hello-git hello");
         System.out.println("hello dev4");
+        System.out.println("push test");
     }
 }
